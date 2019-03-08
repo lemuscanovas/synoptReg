74aa31a478ca45c83ee342ceebcb03fb6cb81cd9
Marc Lemus-Canovas <43061700+lemuscanovas@users.noreply.github.com>
2019-03-08 20:18:07 +0100

    Updated download_ncep.R

--- R/download_ncep.R
+++ R/download_ncep.R
@@ -1,18 +1,19 @@
 #' Download NCEP
 #'
 #' Weather Data from NCEP/NCAR Reanalysis via RNCEP package
-#' 
+#'
 #' @import RNCEP
-#' 
+#'
 #' @param var slp 'sea level pressure' (default) for more variables see help of ?NCEP.gather
 #' @param level surface (default)
 #' @param month_range min,max month c(1,12) (default)
-#' 
-#' @example 
+#' @param hour One or several hours of the following: 0,6,12 or 18 c(0,6) or 1
+#'
+#' @example
 #' #Air temperature 2m for 2017
 #' ta_data <- download_ncep(year_range=2017)
-#' 
-#' 
+#'
+#'
 #' @return a list with: itemize{
 #'    \item{a matrix with the reanalysis data}
 #'    \item{a date-time object}
@@ -27,7 +28,8 @@ download_ncep <- function(var="air.2m",level="gaussian",
                            year_range=c(2010,2017),
                            lat_range=c(30,60),
                            lon_range=c(-30,10),
-                           dailymean=TRUE,
+                           dailymean=FALSE,
+                           hour = NULL,
                            reanalysis2=TRUE,
                            save_download=TRUE,
                            file_name=NULL){
@@ -36,31 +38,74 @@ download_ncep <- function(var="air.2m",level="gaussian",
   data_mat <-        NCEP.gather(var,level,
                                  month_range,
                                  year_range,
-                                  lat_range,
-                                  lon_range,
+                                 lat_range,
+                                 lon_range,
                          reanalysis2 = reanalysis2)
-  
+
+  if(dailymean == TRUE){
+    data_mat <- NCEP.aggregate(data_mat, HOURS=FALSE, fxn='mean')
+
+    lat <- as.numeric(dimnames(data_mat)[[1]])
+    lon <- as.numeric(dimnames(data_mat)[[2]])
+    #expand lonlat for each point
+    lonlat <- expand.grid(lon,lat)
+    names(lonlat) <- c("lat","lon")
+
+    #extract date-time
+    time <- dimnames(data_mat)[[3]]
+    time <- lubridate::ymd(time)
+
+    dimnames(data_mat) <- NULL
+
+    ncep_list <- list(mat=data_mat,time=time,coord=lonlat)
+    return(ncep_list)
+
+
+  } else{
+      data_mat <- data_mat
+  }
+
+  # Specific hour
+  if(is.null(hour)){
+    data_mat <- data_mat
+
+  }else if(!is.null(hour)){
+
+    if(hour == 00){
+      data_mat <-        NCEP.restrict(data_mat,hours2remove = c(06,12,18),set2na = FALSE )
+
+    }else if(hour == 06) {
+     data_mat <-        NCEP.restrict(data_mat,hours2remove = c(00,12,18), set2na = FALSE)
+
+    }else if(hour == 12) {
+      data_mat <-        NCEP.restrict(data_mat,hours2remove = c(00,06,18),set2na = FALSE )
+
+    }else if(hour == 18) {
+      data_mat <-        NCEP.restrict(data_mat,hours2remove = c(00,06,12),set2na = FALSE )
+    }
+  }
+
+
   #extract lonlat
   lat <- as.numeric(dimnames(data_mat)[[1]])
   lon <- as.numeric(dimnames(data_mat)[[2]])
   #expand lonlat for each point
   lonlat <- expand.grid(lon,lat)
   names(lonlat) <- c("lat","lon")
-  
+
   #extract date-time
   time <- dimnames(data_mat)[[3]]
   time <- lubridate::ymd_h(time)
-  
-  dimnames(data_mat) <- NULL
-  
-  ncep_list <- list(mat=data_mat,time=time,coord=lonlat)
- 
-  #in case you want to save the downloaded matrix
-  if(save_download==TRUE) save(ncep_list,file=paste(var,"_grid.RData",sep="")) 
 
+  dimnames(data_mat) <- NULL
+
+  ncep_list <- list(mat=data_mat,time=time,coord=lonlat)
+
+  #in case you want to save the downloaded matrix
+  if(save_download==TRUE) save(ncep_list,file=paste(var,"_grid.RData",sep=""))
 
   return(ncep_list)
-  
+
 }
 
 
